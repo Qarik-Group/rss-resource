27d2baeda319a905d8a66689a26cc73fd687834e
James Hunt <james@niftylogic.com>
2018-02-26 21:05:00 +0000

    Fix printing and date formatting

--- rss/check.go
+++ rss/check.go
@@ -16,12 +16,12 @@ func Check() {
 
 	b, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Printf("Failed to read configuration from standard input: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to read configuration from standard input: %s\n", err)
 		os.Exit(1)
 	}
 	err = json.Unmarshal(b, &cfg)
 	if err != nil {
-		fmt.Printf("Failed to unmarshal configuration: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to unmarshal configuration: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -34,26 +34,26 @@ func Check() {
 	}
 
 	if cfg.Source.URL == "" {
-		fmt.Printf("The (required) source.url parameter was not specified.\n")
+		fmt.Fprintf(os.Stderr, "The (required) source.url parameter was not specified.\n")
 		os.Exit(1)
 	}
 
 	res, err := c.Get(cfg.Source.URL)
 	if err != nil {
-		fmt.Printf("Failed to retrieve %s: %s\n", cfg.Source.URL, err)
+		fmt.Fprintf(os.Stderr, "Failed to retrieve %s: %s\n", cfg.Source.URL, err)
 		os.Exit(1)
 	}
 
 	rss, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Printf("Failed to read response from %s: %s\n", cfg.Source.URL, err)
+		fmt.Fprintf(os.Stderr, "Failed to read response from %s: %s\n", cfg.Source.URL, err)
 		os.Exit(1)
 	}
 
 	out[0].Ref = fmt.Sprintf("%x", sha1.Sum(rss))
 	b, err = json.Marshal(out)
 	if err != nil {
-		fmt.Printf("Failed to marshal output JSON: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to marshal output JSON: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("%s\n", string(b))
--- rss/in.go
+++ rss/in.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func In() {
@@ -19,12 +20,12 @@ func In() {
 
 	b, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Printf("Failed to read configuration from standard input: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to read configuration from standard input: %s\n", err)
 		os.Exit(1)
 	}
 	err = json.Unmarshal(b, &cfg)
 	if err != nil {
-		fmt.Printf("Failed to unmarshal configuration: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to unmarshal configuration: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -37,44 +38,57 @@ func In() {
 	}
 
 	if cfg.Source.URL == "" {
-		fmt.Printf("The (required) source.url parameter was not specified.\n")
+		fmt.Fprintf(os.Stderr, "The (required) source.url parameter was not specified.\n")
 		os.Exit(1)
 	}
 
 	res, err := c.Get(cfg.Source.URL)
 	if err != nil {
-		fmt.Printf("Failed to retrieve %s: %s\n", cfg.Source.URL, err)
+		fmt.Fprintf(os.Stderr, "Failed to retrieve %s: %s\n", cfg.Source.URL, err)
 		os.Exit(1)
 	}
 
 	rss, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Printf("Failed to read response from %s: %s\n", cfg.Source.URL, err)
+		fmt.Fprintf(os.Stderr, "Failed to read response from %s: %s\n", cfg.Source.URL, err)
 		os.Exit(1)
 	}
 	err = ioutil.WriteFile("feed.xml", rss, 0666)
 	if err != nil {
-		fmt.Printf("Failed to save feed.xml to local directory: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to save feed.xml to local directory: %s\n", err)
 		os.Exit(1)
 	}
 
 	err = xml.Unmarshal(rss, &r)
 	if err != nil {
-		fmt.Printf("Failed to parse Atom/RSS feed xml: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to parse Atom/RSS feed xml: %s\n", err)
 		os.Exit(1)
 	}
 
-	os.Mkdir("posts", 0777)
+	root := "."
+	if len(os.Args) > 1 {
+		root = os.Args[1]
+	}
+	root = fmt.Sprintf("%s/posts", root)
+
+	os.Mkdir(root, 0777)
 	for i, item := range r.Items {
-		b, err = json.Marshal(item)
+		ts, err := time.Parse("Mon, 02 Jan 2006 15:04:05 GMT", item.PubDate)
 		if err != nil {
-			fmt.Printf("Failed to marshal post #%d (%s) to JSON: %s\n", i+1, item.Title, err)
+			fmt.Fprintf(os.Stderr, "Failed to parse pubDate '%s': %s\n", item.PubDate, err)
 			os.Exit(1)
 		}
-		file := fmt.Sprintf("posts/%s.json", filename(item.PubDate, item.Title))
+
+		b, err = json.Marshal(item)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to marshal post #%d (%s) to JSON: %s\n", i+1, item.Title, err)
+			os.Exit(1)
+		}
+		file := fmt.Sprintf("%s/%d-%s.json", root, ts.Unix(), filename(item.PubDate, item.Title))
+		fmt.Fprintf(os.Stderr, "- writing %s...\n", file)
 		err = ioutil.WriteFile(file, b, 0666)
 		if err != nil {
-			fmt.Printf("Failed to write post #%d (%s) JSON to %s: %s\n", i+1, item.Title, file, err)
+			fmt.Fprintf(os.Stderr, "Failed to write post #%d (%s) JSON to %s: %s\n", i+1, item.Title, file, err)
 			os.Exit(1)
 		}
 	}
@@ -88,7 +102,7 @@ func In() {
 
 	b, err = json.Marshal(out)
 	if err != nil {
-		fmt.Printf("Failed to marshal output JSON: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to marshal output JSON: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("%s\n", string(b))
